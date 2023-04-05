6311cfb03854010561dff3e0cbbbd66dd1897f35
RAM SHARAN <rsharan30aug@gmail.com>
2023-04-05 16:55:55 +0530

    First Commit

---
+++ src/test/java/com/rs30/restservices/springboot/buildingblocks/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rs30.restservices.springboot.buildingblocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
